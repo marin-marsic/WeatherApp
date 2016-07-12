658b75a7eb654ee8bde4ed823661651bac4c5ad8
marin_marsic <marin.marsic@outlook.com>
2016-07-12 15:17:08 +0200

    Added config

---
+++ WeatherApplication/app/src/main/java/eu/fiveminutes/android/weatherapp/config/Config.java
@@ -0,0 +1,7 @@
+package eu.fiveminutes.android.weatherapp.config;
+
+
+public class Config {
+    public static final String API_KEY = "e1bf45d272617b419b23aa1f3f4d7b3e";
+    public static final String API_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?units=metric";
+}
