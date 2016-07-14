ba0179f0184a116656a68a7dd9e385fa24a96cc6
marin_marsic <marin.marsic@outlook.com>
2016-07-14 10:10:16 +0200

    some implementation errors fixed

--- WeatherApplication/app/src/main/java/eu/fiveminutes/android/weatherapp/parser/JSONWeatherParser.java
+++ WeatherApplication/app/src/main/java/eu/fiveminutes/android/weatherapp/parser/JSONWeatherParser.java
@@ -10,58 +10,57 @@ import eu.fiveminutes.android.weatherapp.model.Weather;
 import eu.fiveminutes.android.weatherapp.model.WeatherDescription;
 import eu.fiveminutes.android.weatherapp.model.WeatherStats;
 
-public class JSONWeatherParser {
+public final class JSONWeatherParser {
 
-    public static final String CITY = "city";
-    public static final String CITY_ID = "id";
-    public static final String CITY_NAME = "name";
-    public static final String CITY_COUNTRY = "coutry";
+    private static final String CITY = "city";
+    private static final String CITY_ID = "id";
+    private static final String CITY_NAME = "name";
+    private static final String CITY_COUNTRY = "coutry";
 
-    public static final String DAYS_LIST = "list";
-    public static final int TODAY = 0;
+    private static final String DAYS_LIST = "list";
+    private static final int TODAY = 0;
 
-    public static final String TEMPERATURE = "temp";
-    public static final String TEMPERATURE_DAY = "day";
-    public static final String TEMPERATURE_MIN = "min";
-    public static final String TEMPERATURE_MAX = "max";
+    private static final String TEMPERATURE = "temp";
+    private static final String TEMPERATURE_DAY = "day";
+    private static final String TEMPERATURE_MIN = "min";
+    private static final String TEMPERATURE_MAX = "max";
 
-    public static final String WEATHER_DESCRIPTION = "weather";
-    public static final int FIRST = 0;
-    public static final String WEATHER_DESCRIPTION_ID = "id";
-    public static final String WEATHER_DESCRIPTION_ICON = "icon";
-    public static final String WEATHER_DESCRIPTION_SHORT = "main";
-    public static final String WEATHER_DESCRIPTION_LONG = "description";
+    private static final String WEATHER_DESCRIPTION = "weather";
+    private static final int FIRST = 0;
+    private static final String WEATHER_DESCRIPTION_ID = "id";
+    private static final String WEATHER_DESCRIPTION_ICON = "icon";
+    private static final String WEATHER_DESCRIPTION_SHORT = "main";
+    private static final String WEATHER_DESCRIPTION_LONG = "description";
 
-    public static final String PRESSURE = "pressure";
-    public static final String HUMIDITY = "humidity";
-    public static final String WIND_SPEED = "speed";
-    public static final String CLOUDS = "clouds";
+    private static final String PRESSURE = "pressure";
+    private static final String HUMIDITY = "humidity";
+    private static final String WIND_SPEED = "speed";
+    private static final String CLOUDS = "clouds";
 
     public Weather getWeather(JSONObject jsonObject) {
 
         // location info
-        JSONObject locationObject = JSONUtil.getSubNode(CITY, jsonObject);
-        Location location = getLocation(locationObject);
+        final JSONObject locationObject = JSONUtil.getSubNode(CITY, jsonObject);
+        final Location location = getLocation(locationObject);
 
         // get info for today
-        JSONArray days = JSONUtil.getJSONArray(DAYS_LIST, jsonObject);
-        JSONObject todayObject = JSONUtil.getJsonObjectFromJsonArray(TODAY, days);
+        final JSONArray days = JSONUtil.getJSONArray(DAYS_LIST, jsonObject);
+        final JSONObject todayObject = JSONUtil.getJsonObjectFromJsonArray(TODAY, days);
 
         // temperature info
-        JSONObject temperatureObject = JSONUtil.getSubNode(TEMPERATURE, todayObject);
-        Temperature temperature = getTemperature(temperatureObject);
+        final JSONObject temperatureObject = JSONUtil.getSubNode(TEMPERATURE, todayObject);
+        final Temperature temperature = getTemperature(temperatureObject);
 
         // weather description
-        JSONArray weatherDescriptionArray = JSONUtil.getJSONArray(WEATHER_DESCRIPTION, todayObject);
-        JSONObject weatherDescriptionObject = JSONUtil.getJsonObjectFromJsonArray(FIRST, weatherDescriptionArray);
-        WeatherDescription weatherDescription = getWeatherDescription(weatherDescriptionObject);
+        final JSONArray weatherDescriptionArray = JSONUtil.getJSONArray(WEATHER_DESCRIPTION, todayObject);
+        final JSONObject weatherDescriptionObject = JSONUtil.getJsonObjectFromJsonArray(FIRST, weatherDescriptionArray);
+        final WeatherDescription weatherDescription = getWeatherDescription(weatherDescriptionObject);
 
         // weather stats
-        WeatherStats weatherStats = getWeatherStats(todayObject);
+        final WeatherStats weatherStats = getWeatherStats(todayObject);
 
 
-        Weather weather = new Weather(location, temperature, weatherStats, weatherDescription);
-        return weather;
+        return new Weather(location, temperature, weatherStats, weatherDescription);
     }
 
     public Weather getWeather(String jsonData) {
@@ -70,44 +69,40 @@ public class JSONWeatherParser {
 
     private Location getLocation(JSONObject jsonObject) {
 
-        String id = JSONUtil.getString(CITY_ID, jsonObject);
-        String city = JSONUtil.getString(CITY_NAME, jsonObject);
-        String country = JSONUtil.getString(CITY_COUNTRY, jsonObject);
+        final String id = JSONUtil.getString(CITY_ID, jsonObject);
+        final String city = JSONUtil.getString(CITY_NAME, jsonObject);
+        final String country = JSONUtil.getString(CITY_COUNTRY, jsonObject);
 
-        Location location = new Location(id, city, country);
-        return location;
+        return new Location(id, city, country);
     }
 
     private Temperature getTemperature(JSONObject jsonObject) {
 
-        int day = JSONUtil.getInt(TEMPERATURE_DAY, jsonObject);
-        int min = JSONUtil.getInt(TEMPERATURE_MIN, jsonObject);
-        int max = JSONUtil.getInt(TEMPERATURE_MAX, jsonObject);
+        final int day = JSONUtil.getInt(TEMPERATURE_DAY, jsonObject);
+        final int min = JSONUtil.getInt(TEMPERATURE_MIN, jsonObject);
+        final int max = JSONUtil.getInt(TEMPERATURE_MAX, jsonObject);
 
-        Temperature temperature = new Temperature(min, max, day);
-        return temperature;
+        return new Temperature(min, max, day);
     }
 
     private WeatherDescription getWeatherDescription(JSONObject jsonObject) {
 
-        String id = JSONUtil.getString(WEATHER_DESCRIPTION_ID, jsonObject);
-        String shortDesc = JSONUtil.getString(WEATHER_DESCRIPTION_SHORT, jsonObject);
-        String longDesc = JSONUtil.getString(WEATHER_DESCRIPTION_LONG, jsonObject);
-        String icon = JSONUtil.getString(WEATHER_DESCRIPTION_ICON, jsonObject);
+        final String id = JSONUtil.getString(WEATHER_DESCRIPTION_ID, jsonObject);
+        final String shortDesc = JSONUtil.getString(WEATHER_DESCRIPTION_SHORT, jsonObject);
+        final String longDesc = JSONUtil.getString(WEATHER_DESCRIPTION_LONG, jsonObject);
+        final String icon = JSONUtil.getString(WEATHER_DESCRIPTION_ICON, jsonObject);
 
-        WeatherDescription weatherDescription = new WeatherDescription(id, longDesc, shortDesc, icon);
-        return weatherDescription;
+        return new WeatherDescription(id, longDesc, shortDesc, icon);
     }
 
     private WeatherStats getWeatherStats(JSONObject jsonObject) {
 
-        int pressure = JSONUtil.getInt(PRESSURE, jsonObject);
-        int humidity = JSONUtil.getInt(HUMIDITY, jsonObject);
-        int windSpeed = JSONUtil.getInt(WIND_SPEED, jsonObject);
-        int cloudness = JSONUtil.getInt(CLOUDS, jsonObject);
+        final int pressure = JSONUtil.getInt(PRESSURE, jsonObject);
+        final int humidity = JSONUtil.getInt(HUMIDITY, jsonObject);
+        final int windSpeed = JSONUtil.getInt(WIND_SPEED, jsonObject);
+        final int cloudness = JSONUtil.getInt(CLOUDS, jsonObject);
 
-        WeatherStats weatherStats = new WeatherStats(humidity, pressure, windSpeed, cloudness);
-        return weatherStats;
+        return new WeatherStats(humidity, pressure, windSpeed, cloudness);
     }
 
 }
