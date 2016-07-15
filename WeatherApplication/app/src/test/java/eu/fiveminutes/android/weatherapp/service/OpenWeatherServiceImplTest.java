77d746fc61b237ecced54d2c650b19d93d7edd89
marin_marsic <marin.marsic@outlook.com>
2016-07-15 14:57:13 +0200

    fixed variable naming

--- WeatherApplication/app/src/test/java/eu/fiveminutes/android/weatherapp/service/OpenWeatherServiceImplTest.java
+++ WeatherApplication/app/src/test/java/eu/fiveminutes/android/weatherapp/service/OpenWeatherServiceImplTest.java
@@ -18,8 +18,8 @@ public class OpenWeatherServiceImplTest {
     public void testGetWeatherRetrofit() throws Exception {
 
         // test depends on network connection and changing weather
-        OpenWeatherService ops = new OpenWeatherServiceImpl();
-        ops.getWeatherForCity("Zagreb", new CallbackImplTest());
+        OpenWeatherService openWeatherService = new OpenWeatherServiceImpl();
+        openWeatherService.getWeatherForCity("Zagreb", new CallbackImplTest());
 
         // wait for async task
         Thread.sleep(2000);
@@ -35,11 +35,11 @@ public class OpenWeatherServiceImplTest {
         @Override
         public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
 
-            WeatherResponse wr = response.body();
-            assertEquals("200", wr.code);
-            assertEquals("Zagreb", wr.city.name);
-            assertEquals("Clouds", wr.days.get(0).descriptionList.get(0).shortDescription);
-            assertEquals(16.52, wr.days.get(0).temperature.min, DELTA);
+            WeatherResponse weatherResponse = response.body();
+            assertEquals("200", weatherResponse.code);
+            assertEquals("Zagreb", weatherResponse.city.name);
+            assertEquals("Clouds", weatherResponse.days.get(0).descriptionList.get(0).shortDescription);
+            assertEquals(16.52, weatherResponse.days.get(0).temperature.min, DELTA);
         }
     }
 }
